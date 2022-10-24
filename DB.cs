e60f22fb080712f6eb0a97392185734af5d532ed
gremc <gremc@DESKTOP-6ESNFKD>
2022-10-24 17:49:14 +0300

    Добавьте файлы проекта.

---
+++ DB.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost; port=3306;username=id19759019_grim;password=k(QwvT-t<4@&@ZEg; database=id19759019_store_stock");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
